cf70d3e107a03f9cc854a95aae589f867c59b301
TegarAlfaR <tegaralfa18052004@gmail.com>
2025-05-10 21:35:37 +0700

    init laundry frontend project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <div className="w-full ">
+        <h1 className="text-center text-2xl">Laundry Antar Jemput</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
