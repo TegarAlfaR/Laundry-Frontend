99ca936ee24442856356879509a8b5e86bb5018b
TegarAlfaR <tegaralfa18052004@gmail.com>
2025-05-18 16:34:13 +0700

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div>
-      <div className="w-full ">
-        <h1 className="text-center text-2xl">Laundry Antar Jemput</h1>
+    <>
+      <Navbar />
+      <Hero />
+      <div>
+        <div className="w-full ">
+          <h1 className="text-center text-2xl">Laundry Antar Jemput</h1>
+          <h1 className="text-center text-2xl mt-[1000px]">
+            Laundry Antar Jemput
+          </h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
